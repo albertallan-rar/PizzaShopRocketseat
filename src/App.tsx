8217f82b26191b6b6728c4a5fa8a748fccc8f8a1
Albert Allan <albertallanramos@gmail.com>
2024-10-14 01:37:44 -0300

    Iniciando projeto do pizza shop - criando e configurando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza.Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
+}
